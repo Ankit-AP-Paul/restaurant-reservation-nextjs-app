5aae9356ea960345da5bc5d31d9e459303cf39aa
ANKIT <ankitpaul894@gmail.com>
2023-08-01 20:33:30 +0530

    minor

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,26 +1,22 @@
-import NavBar from "../components/NavBar";
 import Header from "./components/Header";
 import SearchSideBar from "./components/SearchSideBar";
 import RestaurantCard from "./components/RestaurantCard";
 
 export default function Search() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <Header />
-        <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-          <SearchSideBar />
-          <div className="w-5/6 ml-3">
-            {/* RESAURANT CARD */}
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            {/* RESAURANT CARD */}
-          </div>
+    <>
+      <Header />
+      <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+        <SearchSideBar />
+        <div className="w-5/6 ml-3">
+          {/* RESAURANT CARD */}
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          {/* RESAURANT CARD */}
         </div>
-      </main>
-    </main>
+      </div>
+    </>
   );
 }

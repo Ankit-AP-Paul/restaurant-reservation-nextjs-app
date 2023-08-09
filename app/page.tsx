644202938c7249d7c3d70af02ee7a4f8f5847d95
ANKIT <ankitpaul894@gmail.com>
2023-08-09 21:54:20 +0530

    fetching home page cards data

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,47 @@
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
+import { PrismaClient, Cuisine, Location, PRICE } from "@prisma/client";
 
-export default function Home() {
+export interface RestaurantCardType {
+  id: number;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine;
+  locaton: Location;
+  price: PRICE;
+  slug: string;
+}
+
+const prisma = new PrismaClient();
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      slug: true,
+      locaton: true,
+      price: true,
+    },
+  });
+  return restaurants;
+};
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants();
+
   return (
     <main>
       <Header />
       {/* CARDS */}
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
+        {restaurants.map((restaurant) => (
+          <RestaurantCard restaurant={restaurant} />
+        ))}
       </div>
       {/* CARDS */}
     </main>
   );
 }
-
-// supabase password: yX5wWPlZV2Ti3fRX

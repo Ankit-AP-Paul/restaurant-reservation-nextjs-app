52947f5c6e0275c5d7b255ee98b16ba3efffb925
ANKIT <ankitpaul894@gmail.com>
2023-08-17 07:10:28 +0530

    added restaurant search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,20 +1,56 @@
 import Header from "./components/Header";
 import SearchSideBar from "./components/SearchSideBar";
 import RestaurantCard from "./components/RestaurantCard";
+import { PrismaClient } from "@prisma/client";
 
-export default function Search() {
+const prisma = new PrismaClient();
+
+const fetchRestaurantsByCity = (city: string | undefined) => {
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    locaton: true,
+    slug: true,
+  };
+
+  if (!city) return prisma.restaurant.findMany({ select });
+  return prisma.restaurant.findMany({
+    where: {
+      locaton: {
+        name: {
+          equals: city.toLowerCase(),
+        },
+      },
+    },
+    select,
+  });
+};
+
+export default async function Search({
+  searchParams,
+}: {
+  searchParams: { city: string };
+}) {
+  const restaurants = await fetchRestaurantsByCity(searchParams.city);
+
   return (
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
         <SearchSideBar />
         <div className="w-5/6 ml-3">
-          {/* RESAURANT CARD */}
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          {/* RESAURANT CARD */}
+          {restaurants.length ? (
+            <>
+              {restaurants.map((restaurant) => (
+                <RestaurantCard restaurant={restaurant} />
+              ))}
+            </>
+          ) : (
+            <p>Sorry, no restaurant was found in the area</p>
+          )}
         </div>
       </div>
     </>

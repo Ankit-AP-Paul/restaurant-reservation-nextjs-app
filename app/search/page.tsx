9c3c18a1d7565c6ef3da6e210add34d3f7f95a45
ANKIT <ankitpaul894@gmail.com>
2023-08-18 07:04:06 +0530

    fetch data for rendering sidebar dynamically

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -29,23 +29,32 @@ const fetchRestaurantsByCity = (city: string | undefined) => {
   });
 };
 
+const fetchLocation = async () => {
+  return prisma.location.findMany();
+};
+
+const fetchCuisine = async () => {
+  return prisma.cuisine.findMany();
+};
+
 export default async function Search({
   searchParams,
 }: {
   searchParams: { city: string };
 }) {
   const restaurants = await fetchRestaurantsByCity(searchParams.city);
-
+  const location = await fetchLocation();
+  const cuisine = await fetchCuisine();
   return (
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-        <SearchSideBar />
+        <SearchSideBar locations={location} cuisines={cuisine} />
         <div className="w-5/6 ml-3">
           {restaurants.length ? (
             <>
               {restaurants.map((restaurant) => (
-                <RestaurantCard restaurant={restaurant} />
+                <RestaurantCard restaurant={restaurant} key={restaurant.id} />
               ))}
             </>
           ) : (

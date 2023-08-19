f65afdd429d5d690ea5ae1b800389aaabaefcdda
ANKIT <ankitpaul894@gmail.com>
2023-08-19 17:45:19 +0530

    added background image in resturant page

--- app/restaurant/[slug]/components/Header.tsx
+++ app/restaurant/[slug]/components/Header.tsx
@@ -1,16 +1,45 @@
-export default function Header({ name }: { name: string }) {
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const fetchMainImage = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      main_image: true,
+    },
+  });
+  if (!restaurant) {
+    throw new Error("Can't find a restaurant");
+  }
+  return restaurant.main_image;
+};
+
+export default async function Header({ name }: { name: string }) {
   const renderTitle = () => {
     const nameArr = name.split("-");
     nameArr[nameArr.length - 1] = `(${nameArr[nameArr.length - 1]})`;
     return nameArr.join(" ");
   };
+  const mainImage = await fetchMainImage(name);
+  console.log(mainImage);
 
   return (
     <div className="h-96 overflow-hidden">
-      <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
-        <h1 className="text-7xl text-white capitalize text-shadow text-center">
-          {renderTitle()}
-        </h1>
+      <div
+        className="h-full"
+        style={{
+          backgroundImage: `url(${mainImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}>
+        <div className=" bottom-0 px-4 py-3 bg-gray-500/50 w-full h-full flex items-center justify-center">
+          <h1 className="text-7xl text-white capitalize text-shadow">
+            {renderTitle()}
+          </h1>
+        </div>
       </div>
     </div>
   );

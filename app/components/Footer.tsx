3e38c78943507e9bcd773eac9bc9f25713e60937
ANKIT <ankitpaul894@gmail.com>
2023-08-20 08:16:13 +0530

    Footer to be added

---
+++ app/components/Footer.tsx
@@ -0,0 +1,22 @@
+import { Grid } from "@mui/material";
+
+export default function Footer() {
+  return (
+    <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2 text-white flex items-center ">
+      {/* <Grid container spacing={3}>
+        <Grid item xs={5}>
+          <h1 className="text-2xl">TheUrbanSpoon</h1>
+        </Grid>
+        <Grid item xs={2}>
+          <h3>Permalinks</h3>
+        </Grid>
+        <Grid item xs={2}>
+          <h3>Insights</h3>
+        </Grid>
+        <Grid item xs={3}>
+          <h3>Get In Touch</h3>
+        </Grid>
+      </Grid> */}
+    </div>
+  );
+}
